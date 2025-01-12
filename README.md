3f51a9afb4888334a8f9d5123530633be4df79a2
comba <36135770+Comba92@users.noreply.github.com>
2025-01-13 00:10:38 +0100

    Updated readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# OSDev - Bootloader and kernel
+This repo contains a simple bootloader implementation and a simple kernel in C which prints some text to the screen. 
--- kernel_entry.asm
+++ kernel_entry.asm
@@ -1,7 +1,7 @@
 [bits 32]
 [extern main]
 
-; only porpuose of this is to get it liked at the top of the final executable
+; only porpuose of this is to get it linked at the top of the final executable
 ; so we start the program by jumping to the main function
 call main
 hlt
